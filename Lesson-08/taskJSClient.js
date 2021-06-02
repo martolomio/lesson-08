618e43a5af8262fbf55e2a4dcb7c472a38347c16
martha <partutamarta@gmail.com>
2021-06-02 10:08:49 +0300

    Events in js

---
+++ Lesson-08/taskJSClient.js
@@ -0,0 +1,10 @@
+
+btn.onclick = function() {
+	document.getElementById("text").style.visibility = "hidden";
+	document.getElementById("head").style.display = "none";
+	}
+
+hidde.onclick = function(){
+	this.classList.add('display');
+	}
+	 
